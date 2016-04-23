bb2b4d67a40e29c4f2481d8d74b1fc6b8140a848
Natalie Loh <hi@natalieloh.com>
2016-04-24 00:39:50 +0800

    Added function which compiles handlebar email template

--- hubble_mailer_worker.js
+++ hubble_mailer_worker.js
@@ -10,3 +10,24 @@ var Q = require('q');
 var worker = new iron_worker.Client();
 var imq = new iron_mq.Client({token: "MY_TOKEN", project_id: "MY_PROJECT_ID", queue_name: "MY_QUEUE"})
 console.log("Hello", iron_worker.params()[0]['id'], "!");
+function processTemplate(data) {
+
+    var deferred = Q.defer();
+
+    fs.readFile('templates/hostReminder.html.handlebars', 'utf-8', function(error, source){
+        if (error) return console.log('ERROR READING FILE' + err);
+        
+        var template = handlebars.compile(source, {
+            noEscape: true
+        });
+
+        fs.writeFile('compiledEmail.html', template(data), function (err) {
+          if (err) return console.log('ERROR WRITING FILE' + err);
+        });
+
+        deferred.resolve();
+
+    });
+
+    return deferred.promise;
+}
