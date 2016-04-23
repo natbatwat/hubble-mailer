d0f935880b0f0326c3fa4e8cb7d29b0c50ef799f
Natalie Loh <hi@natalieloh.com>
2016-04-24 00:44:06 +0800

    Code cleanup

--- hubble_mailer_worker.js
+++ hubble_mailer_worker.js
@@ -1,7 +1,6 @@
 var iron_worker = require('iron_worker');
 var iron_mq = require('iron_mq');
 var env = require('node-env-file');
-env(__dirname + '/.env');
 var nodemailer = require('nodemailer');
 var handlebars = require("handlebars");
 var fs = require('fs');
@@ -10,6 +9,8 @@ var Q = require('q');
 var worker = new iron_worker.Client();
 var imq = new iron_mq.Client({token: "MY_TOKEN", project_id: "MY_PROJECT_ID", queue_name: "MY_QUEUE"})
 console.log("Hello", iron_worker.params()[0]['id'], "!");
+env(__dirname + '/.env'); // .env path
+
 var count = 0;
 
 fs.readFile('hubble_mailer.payload.json', 'utf-8', function(error, source) {
