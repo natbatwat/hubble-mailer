c26286f864190c2fe07814ba9285064be90724a3
Natalie Loh <hi@natalieloh.com>
2016-04-24 00:38:51 +0800

    Required npm dependencies
    - handlebars
    - fs
    - Q

--- hubble_mailer_worker.js
+++ hubble_mailer_worker.js
@@ -3,6 +3,9 @@ var iron_mq = require('iron_mq');
 var env = require('node-env-file');
 env(__dirname + '/.env');
 var nodemailer = require('nodemailer');
+var handlebars = require("handlebars");
+var fs = require('fs');
+var Q = require('q');
 
 var worker = new iron_worker.Client();
 var imq = new iron_mq.Client({token: "MY_TOKEN", project_id: "MY_PROJECT_ID", queue_name: "MY_QUEUE"})
